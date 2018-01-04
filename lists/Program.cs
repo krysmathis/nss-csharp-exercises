d6d7a1ab5f72f5de6d6ee531015e799268023030
krysmathis <krysmathis@gmail.com>
2018-01-04 11:48:57 -0600

    completed list exercise

--- lists/Program.cs
+++ lists/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace lists
 {
@@ -7,7 +8,12 @@ namespace lists
     {
         public class Probe {
 
-            public string Name {get; set;}
+        public Probe(string name) 
+        {
+            this.Name = name;
+               
+        }
+                    public string Name {get; set;}
             public List<string> PlanetsVisited {get; set;}
 
             public Probe(string name, List<string> planetsVisited) {
@@ -16,11 +22,18 @@ namespace lists
             }
 
         }
+
         static void Main(string[] args)
         {
             // Lists
-            List<string> planetList = new List<string>(){"Mercury", "Mars", "Jupiter", "Saturn"};
-            List<string> lastPlanets = new List<string>(){"Uranus","Neptune"};
+            List<string> planetList = new List<string>(){
+                "Mercury", "Mars", "Jupiter", "Saturn"
+            };
+
+            List<string> lastPlanets = new List<string>(){
+                "Uranus","Neptune"
+            };
+
             planetList.AddRange(lastPlanets);
             planetList.Insert(1,"Venus");
             planetList.Insert(2,"Earth");
@@ -28,49 +41,57 @@ namespace lists
             List<string> rockyPlanets = planetList.GetRange(0,4);
             planetList.Remove("Pluto");   
 
-            // List of Dictionaries
-            List<Dictionary<string,string>> probes = new List<Dictionary<string,string>>(){};
-            probes.Add(new Dictionary<string,string>{{"Mariner","Mercury"}});
-            probes.Add(new Dictionary<string,string>{{"Mariner","Venus"}});
-            probes.Add(new Dictionary<string,string>{{"Mariner","Mars"}});
-            probes.Add(new Dictionary<string,string>{{"Venera","Venus"}});
-            probes.Add(new Dictionary<string,string>{{"Voyager","Jupiter"}});
-            probes.Add(new Dictionary<string,string>{{"Voyager","Saturn"}});
-            probes.Add(new Dictionary<string,string>{{"Pioneer","Jupiter"}});
 
-            // An alernate approach using a probe object created above, 
-            // this however is not a dictionary
-            List<Probe> probeList = new List<Probe>(){};
-            probeList.Add(new Probe("Maringer",new List<string>() {"Mercury", "Venus", "Mars"}));
-
+            List<Dictionary<string,List<string>>> spacecraft = new List<Dictionary<string,List<string>>>() 
+            {
+                new Dictionary<string,List<string>>() {
+                    {"Mariner", new List<string>(){"Mercury", "Venus", "Mars"}}
+                },
+                new Dictionary<string,List<string>>() {
+                    {"Venera", new List<string>() {"Venus"}}
+                },
+                new Dictionary<string,List<string>>() {
+                    {"Voyager", new List<string>() {"Jupiter", "Saturn"}},
+                },
+                new Dictionary<string,List<string>>() {
+                    {"Pioneer", new List<string>() {"Jupiter"}}
+                }
+            };
+           
             // initialize variables to contain the strings
             string planets = "";
             int c = 0;
             
             planetList.ForEach(planet => {
+                //initialize the output string
                 planets = "";
-                c = 0;
-                probes.ForEach(probe => {
-                    foreach(KeyValuePair<string,string> kvp in probe) {
-                        if (kvp.Value == planet) {
-                        c++;
+                // loop through the list of spacecraft
+                c=0;
+                spacecraft.ForEach(probe => {
+                    // loop through the dictioary of planets visited
+                    foreach(KeyValuePair<string,List<string>> spacecraftKVP in probe) {
+                        string probeName = spacecraftKVP.Key;
+                        List<string> planetsVisited = spacecraftKVP.Value;
+                        if (planetsVisited.Any(p => p.Equals(planet))) {
+                            c++;
                             if (c==1) {
-                                planets = kvp.Key;
+                                planets = probeName;
                             } else {
-                                planets = planets +  ", " + kvp.Key;
+                                planets = planets +  ", " + probeName;
                             }
                         }
                     }
+
                 });
-                // only output results if the planet was visited by a probe
+
+                // now output planets
                 if (planets.Length > 0) {
                     Console.Write($"{planet}: ");
                     Console.WriteLine(planets);
-                } 
+                }
+
             });
-
-
-        
+              
         }
     }
 }
---
+++ tuples/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<(string product, double amount, int quantity)> transactions = new List<(string, double, int)>();
+        
+        
+        
+        }
+    }
+}
